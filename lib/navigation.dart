cf5cabcd342b9ea07f4847e8262b83ab7e3de064
Florian Techer <techerflorian1@gmail.com>
2023-05-09 21:22:09 +0200

    bottom navigation

---
+++ lib/navigation.dart
@@ -0,0 +1,191 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class NavigationPage extends StatefulWidget {
+  @override
+  State<NavigationPage> createState() => _NavigationPageState();
+}
+// final Widget homeIcon = ColorFiltered(
+//     colorFilter: const ColorFilter.mode(Colors.red, BlendMode.srcIn),
+//     child: SvgPicture.asset(
+//       'assets/images/svg/home.svg',
+//   width: 30,
+//   height: 30,
+//     ),
+//   );
+
+final Widget homeIcon = SvgPicture.asset(
+  'assets/images/svg/home.svg',
+  width: 30,
+  height: 30,
+);
+final Widget cartIcon = SvgPicture.asset(
+  'assets/images/svg/cart.svg',
+  width: 30,
+  height: 30,
+);
+final Widget bagIcon = SvgPicture.asset(
+  'assets/images/svg/bag.svg',
+  width: 30,
+  height: 30,
+);
+final Widget heartIcon = SvgPicture.asset(
+  'assets/images/svg/heart.svg',
+  width: 30,
+  height: 30,
+);
+final Widget profileIcon = SvgPicture.asset(
+  'assets/images/svg/profile.svg',
+  width: 30,
+  height: 30,
+);
+int _currentIndex = 0;
+
+class _NavigationPageState extends State<NavigationPage> {
+  Widget build(BuildContext context) {
+    const mainBgColor = Color(0xfff9f9f9);
+    const white = Color(0xffffffff);
+    return BottomAppBar(
+      shape: const CircularNotchedRectangle(),
+      elevation: 0,
+      color: mainBgColor,
+      child: ClipRRect(
+        borderRadius: const BorderRadius.only(
+          topLeft: Radius.circular(16.0),
+          topRight: Radius.circular(16.0),
+        ),
+        child: Container(
+          color: white,
+          height: 56,
+          width: MediaQuery.of(context).size.width,
+          child: Padding(
+            padding: const EdgeInsets.only(left: 25.0, right: 25.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                IconBottomBar(
+                    text: "Home",
+                    icon: Icons.home,
+                    selectedIcon: Icons.home_outlined,
+                    selected: _currentIndex == 0,
+                    onPressed: () => {setState((() => _currentIndex = 0))}),
+                IconBottomBar(
+                    text: "shop",
+                    icon: Icons.shopping_cart,
+                    selectedIcon: Icons.shopping_cart_outlined,
+                    selected: _currentIndex == 1,
+                    onPressed: () => {setState((() => _currentIndex = 1))}),
+                IconBottomBar(
+                    text: "bag",
+                    icon: Icons.shopping_bag,
+                    selectedIcon: Icons.shopping_bag_outlined,
+                    selected: _currentIndex == 2,
+                    onPressed: () => {setState((() => _currentIndex = 2))}),
+                IconBottomBar(
+                    text: "favorite",
+                    icon: Icons.favorite,
+                    selectedIcon: Icons.favorite_outline,
+                    selected: _currentIndex == 3,
+                    onPressed: () => {setState((() => _currentIndex = 3))}),
+                IconBottomBar(
+                    text: "profile",
+                    icon: Icons.person,
+                    selectedIcon: Icons.person_outline_outlined,
+                    selected: _currentIndex == 4,
+                    onPressed: () => {setState((() => _currentIndex = 4))})
+              ],
+            ),
+          ),
+        ),
+      ),
+      
+      // child: SizedBox(
+      //   height: 56,
+      //   width: MediaQuery.of(context).size.width,
+      //   child: Padding(
+      //     padding: const EdgeInsets.only(left: 25.0, right: 25.0),
+      //     child: Row(
+      //       mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      //       crossAxisAlignment: CrossAxisAlignment.start,
+      //       children: [
+      //         IconBottomBar(
+      //             text: "Home",
+      //             icon: Icons.home,
+      //             selectedIcon: Icons.home_outlined,
+      //             selected: _currentIndex == 0,
+      //             onPressed: () => {setState((() => _currentIndex = 0))}),
+      //         IconBottomBar(
+      //             text: "shop",
+      //             icon: Icons.shopping_cart,
+      //             selectedIcon: Icons.shopping_cart_outlined,
+      //             selected: _currentIndex == 1,
+      //             onPressed: () => {setState((() => _currentIndex = 1))}),
+      //         IconBottomBar(
+      //             text: "bag",
+      //             icon: Icons.shopping_bag,
+      //             selectedIcon: Icons.shopping_bag_outlined,
+      //             selected: _currentIndex == 2,
+      //             onPressed: () => {setState((() => _currentIndex = 2))}),
+      //         IconBottomBar(
+      //             text: "favorite",
+      //             icon: Icons.favorite,
+      //             selectedIcon: Icons.favorite_outline,
+      //             selected: _currentIndex == 3,
+      //             onPressed: () => {setState((() => _currentIndex = 3))}),
+      //         IconBottomBar(
+      //             text: "profile",
+      //             icon: Icons.person,
+      //             selectedIcon: Icons.person_outline_outlined,
+      //             selected: _currentIndex == 4,
+      //             onPressed: () => {setState((() => _currentIndex = 4))})
+      //       ],
+      //     ),
+      //   ),
+      // ),
+    );
+  }
+}
+
+class IconBottomBar extends StatelessWidget {
+  const IconBottomBar(
+      {Key? key,
+      required this.text,
+      required this.icon,
+      required this.selectedIcon,
+      required this.selected,
+      required this.onPressed})
+      : super(key: key);
+  final String text;
+  final IconData icon;
+  final IconData selectedIcon;
+  final bool selected;
+  final Function() onPressed;
+
+  final primaryColor = const Color(0xff4338CA);
+  final accentColor = Colors.red;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        IconButton(
+          onPressed: onPressed,
+          icon: Icon(
+            selected ? icon : selectedIcon,
+            size: 25,
+            color: selected ? accentColor : Colors.grey,
+          ),
+        ),
+        Text(
+          text,
+          style: TextStyle(
+              fontSize: 12,
+              height: .1,
+              color: selected ? accentColor : Colors.grey),
+        )
+      ],
+    );
+  }
+}
