8dc463d01fd7f605a3afad860e8adc4d2ac3dab7
Florian Techer <techerflorian1@gmail.com>
2023-05-31 01:44:54 +0200

    add type filter ui

--- lib/pages/catalogsPages/women_catalog.dart
+++ lib/pages/catalogsPages/women_catalog.dart
@@ -18,21 +18,74 @@ class _WomenCatalogPageState extends State<WomenCatalogPage> {
   );
   @override
   Widget build(BuildContext context) {
-    Map<String, dynamic> catalog = widget.catalog; // Accéder à la variable catalog via widget.catalog
+    const white = Color(0xffffffff);
+    List<String> types = List<String>.from(widget.catalog['type']);
+    Map<String, dynamic> catalog =
+        widget.catalog; // Accéder à la variable catalog via widget.catalog
     return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          leading: IconButton(
             icon: backArrow,
             onPressed: () => {Navigator.pop(context)},
           ),
-          title:  Center(child: Text(catalog["title"])),
           actions: [
             IconButton(
               icon: const Icon(Icons.search),
               onPressed: () {},
             ),
           ],
-      ),
-    );
+        ),
+        body: Column(
+          children: [
+            Row(children: [
+              Container(
+                height: 70,
+                alignment: Alignment.bottomLeft,
+                padding: const EdgeInsets.symmetric(horizontal: 15),
+                margin: const EdgeInsets.only(bottom: 20),
+                child: Text(
+                  catalog["title"],
+                  style: const TextStyle(
+                    fontSize: 35,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ]),
+            SizedBox(
+              width: double.infinity,
+              height: 35,
+              // margin: const EdgeInsets.symmetric(horizontal: 10),
+              child: ListView.builder(
+                  physics: const ClampingScrollPhysics(),
+                  scrollDirection: Axis.horizontal,
+                  shrinkWrap: true,
+                  itemCount: types.length,
+                  itemBuilder: (context, index) {
+                    return Container(
+                        width: 90,
+                        height: 35,
+                        margin: index == 0
+                            ? const EdgeInsets.only(left: 10, right: 3)
+                            : index == types.length - 1
+                                ? const EdgeInsets.only(right: 10, left: 3)
+                                : const EdgeInsets.symmetric(horizontal: 3),
+                        padding: const EdgeInsets.only(
+                            left: 7, right: 7, top: 2, bottom: 2),
+                        decoration: BoxDecoration(
+                          color: Colors.black,
+                          borderRadius: BorderRadius.circular(50),
+                        ),
+                        child: Center(
+                            child: Text(
+                          types[index],
+                          style: const TextStyle(fontSize: 14, color: white),
+                        )));
+                  }),
+            )
+          ],
+        ));
   }
 }
